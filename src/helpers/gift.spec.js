6346904a7b79d3370954af0b576c188c9f92bef5
Nguyen An <andinh2601@gmail.com>
2020-05-08 10:49:37 +0700

    fix test

--- src/helpers/gift.spec.js
+++ src/helpers/gift.spec.js
@@ -22,21 +22,21 @@ const initialState = {
     id: 1,
     name: "Test 1 user",
   },
-  gifts: [
-    {
+  gifts: {
+    immer_license: {
       id: "immer_license",
       description: "Immer license",
       image: "https://avatars2.githubusercontent.com/u/45853199?s=400&v=4",
       reservedBy: 2,
     },
-    {
+    egghead_description: {
       id: "egghead_description",
       description: "Egghead license",
       image:
         "https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/231/square_480/EGH_Apollo-GraphQL-React_Final.png",
       reservedBy: undefined,
     },
-  ],
+  },
 };
 
 describe("Reserving an unreserved gift", () => {
@@ -49,12 +49,12 @@ describe("Reserving an unreserved gift", () => {
     },
   });
 
-  test("added a gift to the collection", () => {
-    expect(nextState.gifts.length).toBe(3);
+  test("didn't modify initial-state", () => {
+    expect(Object.keys(initialState.gifts).length).toBe(2);
   });
 
-  test("didn't modify initial-state", () => {
-    expect(initialState.gifts.length).toBe(2);
+  test("added a gift to the collection", () => {
+    expect(Object.keys(nextState.gifts).length).toBe(3);
   });
 });
 
@@ -65,22 +65,28 @@ describe("Reserving an unreserved gift", () => {
   });
 
   test("correctly stores reservedBy", () => {
-    expect(nextState.gifts[1].reservedBy).toBe(1);
+    expect(nextState.gifts["egghead_description"].reservedBy).toBe(1);
   });
 
-  test("didn't modify an original state", () => {
-    expect(initialState.gifts[1].reservedBy).toBe(undefined);
+  test("didn't the original state", () => {
+    expect(initialState.gifts["egghead_description"].reservedBy).toBe(
+      undefined
+    );
   });
 
   test("does structurally share unchanged parts of state tree", () => {
     expect(nextState).not.toBe(initialState);
-    expect(nextState.gifts[1]).not.toBe(initialState.gifts[1]);
-    expect(nextState.gifts[0]).toBe(initialState.gifts[0]);
+    expect(nextState.gifts["egghead_description"]).not.toBe(
+      initialState.gifts["egghead_description"]
+    );
+    expect(nextState.gifts["immer_license"]).toBe(
+      initialState.gifts["immer_license"]
+    );
   });
 
   test("can't accidentally modify the produces state", () => {
     expect(() => {
-      nextState.gifts[1].reservedBy = undefined;
+      nextState.gifts["egghead_description"].reservedBy = undefined;
     }).toThrow();
   });
 });
@@ -92,14 +98,14 @@ describe("Reserving an unreserved gift with patches", () => {
   });
 
   test("correctly stores reservedBy", () => {
-    expect(nextState.gifts[1].reservedBy).toBe(1);
+    expect(nextState.gifts["egghead_description"].reservedBy).toBe(1);
   });
 
   test("generates the correct patches", () => {
     expect(patches).toEqual([
       {
         op: "replace", // `remove` or `add`
-        path: ["gifts", 1, "reservedBy"],
+        path: ["gifts", "egghead_description", "reservedBy"],
         value: 1,
       },
     ]);
@@ -126,12 +132,20 @@ describe("Reserving an already unreserved gift", () => {
   });
 
   test("preserves stored reservedBy", () => {
-    expect(nextState.gifts[0].reservedBy).toBe(2);
+    expect(nextState.gifts["immer_license"].reservedBy).toBe(2);
   });
 
-  test("no new gift should be created", () => {
-    expect(nextState.gifts[0]).toEqual(initialState.gifts[0]);
-    expect(nextState.gifts[0]).toBe(initialState.gifts[0]);
+  test("still produces a new gift", () => {
+    expect(nextState.gifts["immer_license"]).toEqual(
+      initialState.gifts["immer_license"]
+    );
+    expect(nextState.gifts["immer_license"]).toBe(
+      initialState.gifts["immer_license"]
+    );
+  });
+
+  test("still produces a new state", () => {
+    expect(nextState).toEqual(initialState);
     expect(nextState).toBe(initialState);
   });
 });
@@ -145,7 +159,9 @@ describe("Can add book [async]", () => {
         book,
       },
     });
-    expect(nextState.gifts[2].description).toBe("Concrete mathematics");
+    expect(nextState.gifts["0201558025"].description).toBe(
+      "Concrete mathematics"
+    );
   });
 
   test("can add 2 books in parallel", async () => {
@@ -167,6 +183,6 @@ describe("Can add book [async]", () => {
 
     // -- short-hand
     const nextState = [book1, book2].reduce(giftsReducer, initialState);
-    expect(nextState.gifts.length).toBe(4);
+    expect(Object.keys(nextState.gifts).length).toBe(4);
   });
 });
